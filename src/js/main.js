5a1d7465996bbe6731e729cca7529b89c1d01e55
hiroyuki_nagata <hiroyuki.nagata@gmail.com>
2023-11-14 15:41:48 +0900

    source push

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+import '../stylus/style.styl'
+import simpleSmoothScroll from './simpleSmoothScroll'
+import $ from "jquery";
+// import { setupCounter } from './counter.js'
+//
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="/assets/vite.svg" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn moresss22
+//     </p>
+//   </div>
+// `
+//
+// setupCounter(document.querySelector('#counter'))
+
+// window.addEventListener('DOMContentLoaded',()=>{
+//   console.log($('h1'));
+// },false);
+
+new simpleSmoothScroll();
